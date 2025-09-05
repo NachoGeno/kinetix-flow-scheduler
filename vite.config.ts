b49729087d3defa819e0d547fc20f27ccbd36a3f
NachoGeno <92696785+NachoGeno@users.noreply.github.com>
2025-09-05 16:22:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
   },
   preview: {
     host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 4173
   },
   plugins: [
     react(),
