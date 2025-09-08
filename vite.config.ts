4f7f8c8035349ac56619439944805bb067f76b8c
NachoGeno <92696785+NachoGeno@users.noreply.github.com>
2025-09-08 08:22:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { componentTagger } from "lovable-tagger"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "::",
-    port: 8080,
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
+    outDir: "dist"
   },
   preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 4173,
   },
-}))
+});
